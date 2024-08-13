34974ed34d7eccd11b3e0535b2bd8d8eadd20143
Sam Li <weisen.li@hotmail.com>
2024-08-13 17:36:23 +0000

    style dashboard menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,6 +8,12 @@ export const metadata: Metadata = {
   title: "Rental Management Platform",
 };
 
+const DASHBOARD_MENUS = [
+  {tl:"Dashboard"},
+  {tl:"Properties"},
+  {tl:"Renters"},
+];
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,16 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="p-4 shadow flex justify-between items-center">
-          <a className="font-bold text-orange-600 text-xl">RMP Logo</a>
-          <nav>Menu</nav>
-        </header>
         <main className="min-h-screen flex">
           <aside className="w-1/4 p-2">
-            <section className="shadow min-h-screen p-2">Sidebar</section>
+            <section className="shadow-lg min-h-screen p-2">
+              {DASHBOARD_MENUS.map((menu, index)=>
+                <a href="" className="block bg-amber-600 p-2 rounded text-white mt-5 duration-300 hover:bg-amber-500" key={menu.tl}>{menu.tl}</a>
+              )}
+            </section>
           </aside>
           <section className="w-4/5 p-2">
-            <section className="shadow min-h-screen p-2">{children}</section>
+            <section className="shadow-lg min-h-screen p-2">{children}</section>
           </section>
         </main>
       </body>
