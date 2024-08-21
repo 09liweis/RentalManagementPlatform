baadf100762ff072e9c6f6fbd2bfe44737937169
Sam Li <weisen.li@hotmail.com>
2024-08-21 18:45:42 +0000

    add decode jwt function and use in properties api routes

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -2,19 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import jwt from "jsonwebtoken";
 import Property from "@/models/property";
+import { decodeToken } from "@/utils/jwt";
 
 export async function GET(request: NextRequest) {
   try {
-    const headersList = headers();
-    const authorization = headersList.get("Authorization");
-    const authToken = authorization?.split(" ")[1];
-    if (!authToken) {
+    const verified = decodeToken(request);
+
+    if (!verified) {
       return NextResponse.json({ err: "Not Login" }, { status: 401 });
     }
-    const tokenSecret = process.env.TOKEN_SECRET!;
-    const decoded = jwt.verify(authToken, tokenSecret) as { userId: string };
 
-    const properties = await Property.find({ user: decoded.userId });
+    const properties = await Property.find({ user: verified.userId });
     return NextResponse.json({ properties }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ err }, { status: 500 });
@@ -25,16 +23,12 @@ export async function POST(request: NextRequest) {
   try {
     const { name } = await request.json();
 
-    const headersList = headers();
-    const authorization = headersList.get("Authorization");
-    const authToken = authorization?.split(" ")[1];
-    if (!authToken) {
+    const verified = decodeToken(request);
+    if (!verified) {
       return NextResponse.json({ err: "Not Login" }, { status: 401 });
     }
-    const tokenSecret = process.env.TOKEN_SECRET!;
-    const decoded = jwt.verify(authToken, tokenSecret) as { userId: string };
-    
-    const newProperty = new Property({ name, user: decoded.userId });
+
+    const newProperty = new Property({ name, user: verified.userId });
     await newProperty.save();
     return NextResponse.json({ msg: "added" }, { status: 200 });
   } catch (err) {
@@ -44,11 +38,14 @@ export async function POST(request: NextRequest) {
 
 export async function PUT(request: NextRequest) {
   try {
-    const { name, token, _id } = await request.json();
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET!) as {
-      userId: string;
-    };
-    await Property.updateOne({ _id }, { name, user: decoded.userId });
+    const { name, _id } = await request.json();
+
+    const verified = decodeToken(request);
+    if (!verified) {
+      return NextResponse.json({ err: "Not Login" }, { status: 401 });
+    }
+
+    await Property.updateOne({ _id }, { name, user: verified.userId });
     return NextResponse.json({ msg: "updated" }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ err }, { status: 500 });
@@ -59,16 +56,12 @@ export async function DELETE(request: NextRequest) {
   try {
     const { _id } = await request.json();
 
-    const headersList = headers();
-    const authorization = headersList.get("Authorization");
-    const authToken = authorization?.split(" ")[1];
-    if (!authToken) {
+    const verified = decodeToken(request);
+    if (!verified) {
       return NextResponse.json({ err: "Not Login" }, { status: 401 });
     }
-    const tokenSecret = process.env.TOKEN_SECRET!;
-    const decoded = jwt.verify(authToken, tokenSecret) as { userId: string };
 
-    await Property.deleteOne({ _id, user: decoded.userId });
+    await Property.deleteOne({ _id, user: verified.userId });
     return NextResponse.json({ msg: "deleted" }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ err }, { status: 500 });
