867ef990de0e0e1aee22bb9ccc2950aebe256e65
Sam Li <weisen.li@hotmail.com>
2024-08-21 03:31:35 +0000

    initial room model and api

--- app/api/properties/[propertyId]/rooms/route.ts
+++ app/api/properties/[propertyId]/rooms/route.ts
@@ -1,10 +1,24 @@
+import Room from "@/models/room";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { propertyId: string } },
-) {
+interface ParamsProps {
+  params: {
+    propertyId: string;
+  };
+}
+
+export async function GET(request: NextRequest, { params }: ParamsProps) {
+  try {
+    const { propertyId } = params;
+    const rooms = Room.find({ property: propertyId });
+    return NextResponse.json({ rooms }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ err }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest, { params }: ParamsProps) {
   const { propertyId } = params;
 
-  return NextResponse.json({ propertyId }, { status: 200 });
+  return NextResponse.json({ msg: "added" }, { status: 200 });
 }
---
+++ models/room.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const roomSchema = new mongoose.Schema({
+  property: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+});
+
+const Room = mongoose.models.rooms || mongoose.model("rooms", roomSchema);
+
+export default Room;
