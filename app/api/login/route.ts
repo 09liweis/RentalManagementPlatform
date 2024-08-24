f628aad6ea3f1233743ea029bb2c10b3a4ed748c
TheClaireLiu <cliu202211@gmail.com>
2024-08-24 10:20:56 -0400

    [Update] Update the page.js of forgot password

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: NextRequest) {
     } else {
       const validPassword = await bcryptjs.compare(
         password,
-        existingUser.password,
+        existingUser.password
       );
 
       if (!validPassword) {
