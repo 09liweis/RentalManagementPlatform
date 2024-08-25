3d2c63a4770639eb7155eca93654bd220f273af1
Sam Li <weisen.li@hotmail.com>
2024-08-25 20:22:08 +0000

    update dashboard card

--- app/(landloard)/dashboard/page.tsx
+++ app/(landloard)/dashboard/page.tsx
@@ -46,6 +46,31 @@ export default function Dashboard() {
           {p.name}
         </Link>
       ))}
+
+      <section className="flex gap-5 flex-wrap">
+        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+          <p>2 Properties</p>
+          <p>8 Rooms</p>
+          <p>7 Tenants</p>
+        </article>
+        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+          <h3>Total Profit</h3>
+          <p>$500</p>
+        </article>
+        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+          <h3>Rent received</h3>
+          <p>$3000</p>
+        </article>
+        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+          <h3>Overdue rent</h3>
+          <p>$300</p>
+        </article>
+        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+          <h3>Utility Cost</h3>
+          <p>$700</p>
+        </article>
+        
+      </section>
       <Map />
     </>
   );
