5890f03a0df04e24ab4bb8c1bb1ed46b7d2b77ee
Sam Li <weisen.li@hotmail.com>
2024-08-26 14:51:56 +0000

    update dashboard card style

--- app/(landloard)/dashboard/page.tsx
+++ app/(landloard)/dashboard/page.tsx
@@ -7,27 +7,27 @@ import Map from "@/components/common/Map";
 export default function Dashboard() {
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1 className="page-title">Dashboard</h1>
 
-      <section className="flex gap-5 flex-wrap">
-        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+      <section className="card-container">
+        <article className="card">
           <p>2 Properties</p>
           <p>8 Rooms</p>
           <p>7 Tenants</p>
         </article>
-        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+        <article className="card">
           <h3>Total Profit</h3>
           <p>$500</p>
         </article>
-        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+        <article className="card">
           <h3>Rent received</h3>
           <p>$3000</p>
         </article>
-        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+        <article className="card">
           <h3>Overdue rent</h3>
           <p>$300</p>
         </article>
-        <article className="shadow p-2 w-full sm:w-1/2 md:w-1/4 lg:1/5">
+        <article className="card">
           <h3>Utility Cost</h3>
           <p>$700</p>
         </article>
