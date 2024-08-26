764932f11e2e7f13cbf209f640ec3aed06dcd893
Sam Li <weisen.li@hotmail.com>
2024-08-26 20:24:13 +0000

    style sidebar

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -1,19 +1,22 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 const DASHBOARD_MENUS = [
   { tl: "Dashboard",path:"/dashboard" },
   { tl: "Properties",path:"/dashboard/properties" },
-  { tl: "Renters",path:"/dashboard/renters" },
+  { tl: "Tenants",path:"/dashboard/tenants" },
 ];
 
 export default function Sidebar() {
+  const curPathname = usePathname()
   return (
     <aside className="w-1/4 p-2">
       <section className="shadow-lg min-h-screen p-2">
         {DASHBOARD_MENUS.map((menu, index) => (
           <Link
             href={menu.path}
-            className="block bg-amber-600 p-2 rounded text-white mt-5 duration-300 hover:bg-amber-500"
+            className={`sidebar-menu ${menu.path === curPathname ? 'active' : ''}`}
             key={menu.tl}
           >
             {menu.tl}
