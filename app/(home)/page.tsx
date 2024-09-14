e0169a2e6cff467af08d4c1dddaf45269a77c096
Sam Li <weisen.li@hotmail.com>
2024-09-14 15:57:03 +0000

    fix home page style

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <>
+    <section className="w-full">
     <section className="hero-container">
       <h1 className="home-page-title">Success starts with a great product</h1>
       <p className="home-page-subtitle">Morbi leo tortor, fermentum sed orci vitae, tempor auctor turpis. Fusce bibendum accumsan fringilla. Nulla aliquam luctus sem, at posuere nibh lobortis et.</p>
@@ -9,6 +9,6 @@ export default function Home() {
       <h1 className="video-container-title">Unlock The Potential</h1>
       <p className="video-container-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer est tellus, pulvinar ac iaculis vel, varius eu arcu. Integer id lectus posuere nisi dignissim gravida at ac justo. Ut nec dui rutrum, cursus mauris sed integer eu vel.</p>
     </section>
-    </>
+    </section>
   );
 }
