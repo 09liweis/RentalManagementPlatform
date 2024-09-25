2c5ed3e892d22e5d97190f558a8661e33b247ac2
09liweis <weisen.li@hotmail.com>
2024-09-25 09:49:04 -0400

    add send email lib

---
+++ lib/email.ts
@@ -0,0 +1,26 @@
+export const sendMail = async () => {
+  const API_KEY: string = process.env.MAILCHIMP_API_KEY as string;
+  const DATACENTER: string = API_KEY?.split('-')[1];
+  const API_URL: string = `https://${DATACENTER}.api.mailchimp.com/3.0/messages/send`;
+
+  const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString('base64');
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic ${base64ApiKey}`,
+  };
+
+  const message = {
+    html:'<h2>Test</h2>',
+    subject:'Test Subject',
+    from_email:'weisen.li@hotmail.com',
+    to:'weisen.li@hotmail.com'
+  }
+
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(message),
+  })
+
+  return response;
+}
