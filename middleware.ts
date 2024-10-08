ebcd5730d5d967e6208bd4ae4fb5e5aa866a3ec5
Sam Li <weisen.li@hotmail.com>
2024-10-08 17:59:17 +0000

    add favicon

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,7 @@ export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl
   
+  if (pathname.startsWith('/favicon')) return;
   if (pathname.startsWith('/api'))  return;
 
   const pathnameHasLocale = i18n.locales.some(
