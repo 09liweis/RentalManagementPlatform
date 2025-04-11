806c49398aedf3c1cbf52e517ae0054f2c512f74
Sam Li <weisen.li@hotmail.com>
2025-04-11 21:09:29 +0000

    style tenant card

--- components/tenant/TenantList.tsx
+++ components/tenant/TenantList.tsx
@@ -17,21 +17,16 @@ export default function TenantList({ loading, tenants, onEditClick,setCurrentTen
 
   return (
     <LoadingSection loading={loading}>
-      <section className="card-container">
+      <section className="card-container grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {tenants.map((tenant) => (
-          <article key={tenant._id} className="card">
-            <div className="flex justify-between">
+          <article key={tenant._id} className="card space-y-2">
+            <div className="flex gap-2 items-center">
+              <div className={`rounded-lg w-5 h-5 cursor-pointer ${tenant.isCurrent?'bg-green-600':'bg-gray-500'}`} onClick={()=>setCurrentTenant(tenant)}></div>
               <LinkText className="tenant-name" href={`/dashboard/tenants/${tenant._id}`} text={tenant.name||''} />
-              <span className="text-red-400 cursor-pointer" onClick={()=>onEditClick(tenant)}>{t('dashboard.Edit')}</span>
             </div>
-            <div className="flex justify-between items-center">
-              <div className={`rounded-lg w-10 h-10 cursor-pointer ${tenant.isCurrent?'bg-green-600':'bg-gray-500'}`} onClick={()=>setCurrentTenant(tenant)}></div>
-              <div>{t('dashboard.Deposit')}: {tenant.deposit}</div>
-              <div>
-                <span className="tenant-date">{tenant.startDate}</span>
-                {tenant.endDate && <span className="tenant-date"> - {tenant.endDate}</span>}
-              </div>
-            </div>
+            <div className="font-bold text-blue-500">{t('dashboard.Deposit')}: ${tenant.deposit}</div>
+            <div className="text-gray-800 italic">{tenant.startDate} - {tenant.endDate || 'unknown'}</div>
+            <div className="text-red-400 cursor-pointer" onClick={()=>onEditClick(tenant)}>{t('dashboard.Edit')}</div>
           </article>
         ))}
       </section>
