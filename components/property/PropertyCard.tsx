8af60c99d9aa1a4b070a5af633779ec1194cc892
Sam Li <weisen.li@hotmail.com>
2025-06-10 03:21:16 +0000

    style propertycard

--- components/property/PropertyCard.tsx
+++ components/property/PropertyCard.tsx
@@ -22,8 +22,7 @@ export default function PropertyCard({
   return (
     <article
       className={`bg-white rounded-xl shadow-sm hover:shadow-md transition-all duration-300 
-                  transform hover:-translate-y-1 border
-                  ${hasSelected ? "p-2" : "p-3"}
+                  transform hover:-translate-y-1 border p-3
                   ${
                     isSelected
                       ? "border-blue-500 ring-2 ring-blue-100"
@@ -38,50 +37,46 @@ export default function PropertyCard({
             href={`/dashboard/properties/${p._id}`}
             text={p.name}
           />
-          {!hasSelected && (
-            <>
-              <div className="flex items-center text-gray-500">
-                <svg
-                  className="w-4 h-4 mr-2"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
-                  />
-                </svg>
-                <span className="text-sm">{p.ptypeTxt}</span>
-              </div>
-              <div className="flex items-center text-gray-500">
-                <svg
-                  className="w-4 h-4 mr-2"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <span className="text-sm max-w-xs">{p.address}</span>
-              </div>
-            </>
-          )}
+          <div className="flex items-center text-gray-500">
+            <svg
+              className="w-4 h-4 mr-2"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
+              />
+            </svg>
+            <span className="text-sm">{p.ptypeTxt}</span>
+          </div>
+          <div className="flex items-center text-gray-500">
+            <svg
+              className="w-4 h-4 mr-2"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            <span className="text-sm max-w-xs">{p.address}</span>
+          </div>
         </div>
         {(isSelected || !hasSelected) && handlePropertyEdit && (
           <Button
