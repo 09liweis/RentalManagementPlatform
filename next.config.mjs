08e25f45bd7f34686ab1ff80709b86cfff637ce8
Sam Li <weisen.li@hotmail.com>
2026-01-04 02:30:51 +0000

    add not found page

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en', 'zh'],
-    defaultLocale: 'en',
-    localeDetection: false,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
