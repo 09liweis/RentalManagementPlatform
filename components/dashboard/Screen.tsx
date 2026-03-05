54f983dfaecc94f2934c471102d740458a7b40f3
Sam Li <weisen.li@hotmail.com>
2026-03-05 20:37:19 +0000

    refactor property rooms api

--- components/dashboard/Screen.tsx
+++ components/dashboard/Screen.tsx
@@ -14,6 +14,7 @@ import usePropertyStore from "@/stores/propertyStore";
 import Properties from "./Properties";
 import RoomList from "../room/RoomList";
 import RoomForm from "../room/RoomForm";
+import { PROPERTY_ROOMS } from "@/constants/apis";
 
 interface ScreenProps {
   propertyId?: string;
@@ -77,7 +78,7 @@ export default function Screen({ propertyId, roomId, tenantId }: ScreenProps) {
     const method = room?._id ? "PUT" : "POST";
     const url = room?._id
       ? `/api/rooms/${room._id}`
-      : `/api/properties/${propertyId}/rooms`;
+      : PROPERTY_ROOMS(propertyId!);
     const { err, msg } = await fetchData({
       url,
       method,
--- constants/apis.ts
+++ constants/apis.ts
@@ -5,3 +5,5 @@ export const USER_DETAIL = '/api/user';
 export const PROPERTY_LIST = '/api/properties';
 
 export const PROPERTY_DETAIL = (propId: string) => `/api/properties/${propId}`;
+
+export const PROPERTY_ROOMS = (propId: string) => `/api/properties/${propId}/rooms`;
