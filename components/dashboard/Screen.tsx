fad7f9b50a036b3d8c05c61bda53a4a3a255d75e
Sam Li <weisen.li@hotmail.com>
2026-03-13 17:26:42 +0000

    update screen component

--- components/dashboard/Screen.tsx
+++ components/dashboard/Screen.tsx
@@ -1,19 +1,8 @@
 "use client";
 
-import { fetchData } from "@/utils/http";
-import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Room } from "@/types/room";
-import { showToast } from "@/components/common/Toast";
 import RentCards from "@/components/dashboard/RentCards";
-import CostForm from "@/components/property/CostForm";
-import CostList from "@/components/dashboard/CostList";
-import useAppStore from "@/stores/appStore";
-import usePropertyStore from "@/stores/propertyStore";
 import Properties from "./Properties";
-import RoomList from "../room/RoomList";
-import RoomForm from "../room/RoomForm";
-import { PROPERTY_ROOMS, ROOM_DETAIL } from "@/constants/apis";
 
 interface ScreenProps {
   propertyId?: string;
@@ -42,55 +31,7 @@ const itemVariants = {
   },
 };
 
-const formVariants = {
-  initial: { opacity: 0, scale: 0.95 },
-  animate: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-      ease: "easeOut",
-    },
-  },
-  exit: {
-    opacity: 0,
-    scale: 0.95,
-    transition: {
-      duration: 0.2,
-    },
-  },
-};
-
 export default function Screen({ propertyId, roomId, tenantId }: ScreenProps) {
-  const { t } = useAppStore();
-  const { costs, rooms, curProperty, curRoom, curTenant, fetchPropertyStats } =
-    usePropertyStore();
-
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {}, []);
-
-  const [room, setRoom] = useState<Room>({ name: "" });
-  const [showRoomForm, setShowRoomForm] = useState(false);
-  const handleRoomSubmit = async (e: any) => {
-    e.preventDefault();
-    const method = room?._id ? "PUT" : "POST";
-    const url = room?._id
-      ? ROOM_DETAIL(room._id)
-      : PROPERTY_ROOMS(propertyId!);
-    const { err, msg } = await fetchData({
-      url,
-      method,
-      body: room,
-    });
-    // fetchPropertyRooms();
-    showToast(err || msg);
-    setRoom({ name: "" });
-    setShowRoomForm(false);
-    fetchPropertyStats({ propertyId });
-  };
-
-  const [showCostForm, setShowCostForm] = useState(false);
 
   return (
     <motion.div
@@ -111,35 +52,6 @@ export default function Screen({ propertyId, roomId, tenantId }: ScreenProps) {
         <Properties />
       </motion.div>
 
-      {curProperty?._id && (
-        <motion.div variants={itemVariants}>
-          <CostList
-            costs={costs}
-            onAddCost={() => setShowCostForm(true)}
-          />
-        </motion.div>
-      )}
-
-      {showCostForm && <CostForm showCostForm={setShowCostForm} />}
-
-      {curProperty?._id && (
-        <motion.div variants={itemVariants}>
-          <RoomList
-            rooms={rooms}
-            setRoom={setRoom}
-            setShowRoomForm={setShowRoomForm}
-          />
-        </motion.div>
-      )}
-
-      {showRoomForm && (
-        <RoomForm
-          room={room}
-          setRoom={setRoom}
-          handleRoomSubmit={handleRoomSubmit}
-          setShowRoomForm={setShowRoomForm}
-        />
-      )}
     </motion.div>
   );
 }
